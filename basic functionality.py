efa545e57399ec86d11baf480fe080052d4c7529
Westpol <81779508+Westpol@users.noreply.github.com>
2023-02-21 18:30:51 +0100

    added submenu with dates to show data

--- basic functionality.py
+++ basic functionality.py
@@ -1,5 +1,6 @@
 import json
 import datetime
+import random
 import time
 import keyboard
 
@@ -15,6 +16,10 @@ class Menu:
         self._print_menu(self._menupoints)
 
     def _print_menu(self, menpoints):
+        if keyboard.is_pressed("enter"):
+            while keyboard.is_pressed("enter"):
+                time.sleep(0.01)
+            menpoints[1] = 1
         if menpoints[0] == 1 and menpoints[1] == 0:
             if keyboard.is_pressed("down"):
                 while keyboard.is_pressed("down"):
@@ -22,15 +27,51 @@ class Menu:
                 self._menupoints[0] += 1
             print("Show data  <")
             print("Add data")
+            print("Reset data")
         elif menpoints[0] == 2 and menpoints[1] == 0:
             if keyboard.is_pressed("up"):
                 while keyboard.is_pressed("up"):
                     time.sleep(0.01)
                 self._menupoints[0] -= 1
+            elif keyboard.is_pressed("down"):
+                while keyboard.is_pressed("down"):
+                    time.sleep(0.01)
+                self._menupoints[0] += 1
             print("Show data")
             print("Add data  <")
-        elif menpoints[0] == 1 and menpoints[1] == 1:
-            pass    
+            print("Reset data")
+        elif menpoints[0] == 3 and menpoints[1] == 0:
+            if keyboard.is_pressed("up"):
+                while keyboard.is_pressed("up"):
+                    time.sleep(0.01)
+                self._menupoints[0] -= 1
+            print("Show data")
+            print("Add data")
+            print("Reset data  <")
+
+        elif menpoints[0] == 1 and menpoints[1] != 0:
+            self._print_dates()
+
+    def _print_dates(self):
+        datess = data.get_dates()
+
+        if keyboard.is_pressed("up"):
+            while keyboard.is_pressed("up"):
+                time.sleep(0.01)
+            if 1 < self._menupoints[1] <= len(dates) + 1:
+                self._menupoints[1] -= 1
+
+        if keyboard.is_pressed("down"):
+            while keyboard.is_pressed("down"):
+                time.sleep(0.01)
+            if 1 <= self._menupoints[1] < len(dates):
+                self._menupoints[1] += 1
+
+        for i in range(len(dates)):
+            if i == self._menupoints[1] - 1:
+                print(datess[i] + "  <")
+            else:
+                print(datess[i])
 
 
 class Data:
@@ -57,6 +98,8 @@ if __name__ == "__main__":
     while 1:
         menu.refresh()
         time.sleep(.05)
+        if random.randint(0, 5000) == 1:
+            break
     print(str(data.get_data(0, "putin")) + " l")
     print(str(round((data.get_data(1, "tachom") - data.get_data(0, "tachom")) / data.get_data(1, "putin"), 2)) + " l/100km")
     print(datetime.date.today().strftime("%d.%m.%Y"))
