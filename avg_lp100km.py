116ac70adadada1f03c3a693831e2ab657eb5e5a
Westpol <81779508+Westpol@users.noreply.github.com>
2023-04-16 18:46:00 +0200

    Added first graph for average liters per 100 km

---
+++ avg_lp100km.py
@@ -0,0 +1,41 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import json
+
+
+class Data:
+    def __init__(self):
+        self._dates = []
+        self._liters = []
+        self._tachometer = []
+        self.dt = json.load(open("data_raw.json"))
+
+    def get_dates(self):
+        date_list = []
+        for i in range(len(self.dt)):
+            date_list.append(self.dt[i]["date"])
+        return date_list
+
+    def get_data(self, index, typ):
+        return self.dt[index][typ]
+
+
+def lp100km(xi):
+    if xi >= 1:
+        return data.get_data(xi, "putin") / ((data.get_data(xi, "tachom") - data.get_data(xi - 1, "tachom")) / 100)
+    return 0
+
+
+if __name__ == "__main__":
+    data = Data()
+
+    tachoms = []
+    indexes = []
+
+    for i in range(0, len(data.get_dates())):
+        indexes.append(i)
+        tachoms.append(lp100km(i))
+
+    plt.plot(indexes, tachoms, color="red")
+
+    plt.show()
